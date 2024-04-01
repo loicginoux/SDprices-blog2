4e2a6b8572f36d3d746622c53512f5fa975f7db9
Loic <lginoux@gmail.com>
2024-04-01 18:03:35 +0200

    change backend by supabase

--- src/utils/searchUrl.js
+++ src/utils/searchUrl.js
@@ -1,1 +1,9 @@
-export const searchUrl = import.meta.env.MODE == "development" ? "http://localhost:4000/api/search" : "https://api.sdprices.com/api/search"
+// export const searchUrl = import.meta.env.MODE == "development" ? "http://localhost:4000/api/search" : "https://api.sdprices.com/api/search"
+
+let searchUrl =  "http://127.0.0.1:54321/functions/v1/search"
+if (import.meta.env.MODE != "development") {
+  searchUrl = "https://tdzlyurkltsgordwhbbq.supabase.co/functions/v1/search"
+
+}
+
+export { searchUrl }
